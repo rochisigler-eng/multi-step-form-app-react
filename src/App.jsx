e8f6fdbee6e1ee124f532a3c0da8567cbafcbf2a
rochi <rochi.sigler@gmail.com>
2026-03-19 19:27:47 -0300

    added structure folders

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import R01MultiStepFormApp from './templates/R01MultiStepFormApp'
+
+function App() {
+
+  return (
+    <>
+      <R01MultiStepFormApp />
+    </>
+  )
+}
+
+export default App
---
+++ src/templates/R01MultiStepFormApp.jsx
@@ -0,0 +1,9 @@
+
+
+const R01MultiStepFormApp = () => {
+  return (
+    <div>R01MultiStepFormApp</div>
+  )
+}
+
+export default R01MultiStepFormApp

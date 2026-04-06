88d79ad25de5fe5e6c8512b7299438ad678e75d3
rochi <rochi.sigler@gmail.com>
2026-04-06 16:05:19 -0300

    all steps slider styles and data array in r01

--- src/organisms/all-steps/AllSteps.jsx
+++ src/organisms/all-steps/AllSteps.jsx
@@ -1,7 +1,26 @@
+import styles from './AllSteps.module.scss'
 
-const AllSteps = () => {
+const AllSteps = ({data}) => {
   return (
-    <div>AllSteps</div>
+    <div className={styles.allSteps}>
+      <ul className={styles.stepList}>
+        {
+          data.map((step)=>{
+            return(
+            <li className={styles.listStep}>
+              <div className={styles.listNumber}>
+                {step.id+1}
+              </div>
+              <div>
+                <p style={{color: '#fff', fontSize: '1rem', textTransform: 'uppercase', fontWeight: '200'}}>Step {step.id+1}</p>
+                <p style={{color: '#fff', fontSize: '1.5rem', textTransform: 'uppercase', fontWeight: '800'}}>{step.stepName}</p>
+              </div>
+            </li>
+            )
+          })
+        }
+      </ul>
+    </div>
   )
 }
 
--- src/templates/R01MultiStepFormApp.jsx
+++ src/templates/R01MultiStepFormApp.jsx
@@ -12,20 +12,65 @@ import styles from './R01MultiStepFormApp.module.scss'
 const R01MultiStepFormApp = () => {
   const {stepDisplay, setStepDisplay} = useState('one')
 
+  const data = [
+    {
+      id:0,
+      stepName: "Your Info",
+      title: "Personal info",
+      description: "Please provide your name, email address, and phone number.",
+      options:["Name", "Email Address", "Phone Number"],
+      optionDescription: ["e.g.Stephen King", "e.g.StephenKing@gmail.com","e.g.11-2345-6789"]
+    },
+    {
+      id:1,
+      stepName: "Select Plan",
+      title: "Select Your Plan",
+      description: "You have the option of monthly or yearly billing.",
+      options:["Arcade", "Advanced", "Pro"],
+      optionDescription: ["9", "12","15"]
+    },
+    {
+      id:2,
+      stepName: "Add-Ons",
+      title: "Pick add-ons",
+      description: "Add-ons help enhance your gaming experience.",
+      options:["Online service", "Larger storage", "Customizable profile"],
+      optionDescription: ["Access to multiplayer games", "Extra 1TB of cloud save","Custom theme on your profile"]
+    },
+    {
+      id:3,
+      stepName: "Summary",
+      title: "Finishing up",
+      description: "Double-check everything looks OK before confirming.",
+      options:["", "", ""],
+      optionDescription: ["", "",""]
+    }
+  ]
+
   return (
-    <div className={styles.formContainer}>
+    <form className={styles.formContainer}>
       <div className={styles.formPath}>
-      <AllSteps />
+      <AllSteps
+      data={data}
+      />
       </div>
       <div className={styles.StepInformation}>
-      <StepOne />
-      {/* <StepTwo />
-      <StepThree />
-      <StepFour />
+      <StepOne
+      data={data}
+      />
+      {/* <StepTwo
+      data={data}
+      />
+      <StepThree
+      data={data}
+      />
+      <StepFour
+      data={data}
+      />
       <StepFive /> */}
       <StepButtons />
       </div>
-    </div>
+    </form>
   )
 }
 
